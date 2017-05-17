f4277870007a38a3e058b9e7b5476a31a21d630c
Dmitry Vinogradov <di.vinogradov@gmail.com>
2017-05-17 20:47:21 +0300

    1st level
    Moving to the stairs implemented

--- ruby warrior.rb
+++ ruby warrior.rb
@@ -1,7 +1,29 @@
 
 class Player
-  def play_turn(warrior)
-    # cool code goes here
-  end
+	# Инициализация
+	def initialize
+	end
+ 
+	# Метод выполнения шага игры
+    def play_turn(warrior)
+		turn = WarriorTurn.new(warrior)
+		turn.go!
+  	end
+end
+
+class WarriorTurn < SimpleDelegator
+
+	def go!
+		# Пойдем к леснице, если ничего больше не остается
+		move_to_stairs! if nothing_there?	
+	end
+
+	def nothing_there?
+		true
+	end
+
+	def move_to_stairs!
+		self.walk!(self.direction_of_stairs)
+	end
 end
   
